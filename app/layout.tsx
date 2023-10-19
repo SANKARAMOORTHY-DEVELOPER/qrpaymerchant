5deb58a3986dfe8d324f484e1b80186a5a1e4fbf
SANKARAMOORTHY-DEVELOPER <78921146+SANKARAMOORTHY-DEVELOPER@users.noreply.github.com>
2023-10-19 23:45:53 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ app/layout.tsx
@@ -0,0 +1,55 @@
+import Navbar from '@/components/Navbar';
+import './globals.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import Footer from '@/components/Footer';
+import { Analytics } from '@vercel/analytics/react';
+
+const inter = Inter({ subsets: ['latin'] });
+
+let title = 'QrGPT - QR Code Generator';
+let description = 'Generate your AI QR Code in seconds';
+let url = 'https://www.qrgpt.io';
+let ogimage = 'https://www.qrgpt.io/og-image.png';
+let sitename = 'qrGPT.io';
+
+export const metadata: Metadata = {
+  metadataBase: new URL(url),
+  title,
+  description,
+  icons: {
+    icon: '/favicon.ico',
+  },
+  openGraph: {
+    images: [ogimage],
+    title,
+    description,
+    url: url,
+    siteName: sitename,
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    images: [ogimage],
+    title,
+    description,
+  },
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Navbar />
+        <main>{children}</main>
+        <Analytics />
+        <Footer />
+      </body>
+    </html>
+  );
+}
